810f48f41478380af4e6d9056fbf426376192750
ParthGanjoo <108531199+ParthGanjoo@users.noreply.github.com>
2025-02-28 19:00:52 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -20,7 +20,7 @@ function About() {
                     </div>
                     <div class="mmh text-item seven columns">
                         <p>
-                        Hi, I’m Parth Ganjoo, a 21 year old Computer Science and Design student at IIIT Delhi. I'm a Frontend and XR Developer, UI/UX Designer and a Wildlife Photographer.
+                        Hi, I’m Parth Ganjoo, a 21 year old Computer Science and Design student at IIIT Delhi. I'm a Product Designer, Frontend and XR Developer, and a Wildlife Photographer.
                         </p>
                         <p>
                         Birdwatching since 2011, photographing wildlife since 2018 and passionate about designing things since forever, my interest in photography and design has been ever growing. I am always enthusiastic about going out in the wild and taking photos, involved in learning more about design and picking up new skills and knowledge. Building upon this passion for design, thanks to my technical background, I learned to design and develop websites and extended reality interfaces. I also play my guitar and sing sometimes. (P.S. The Moon on the home page is shot by me on my terrace!)
